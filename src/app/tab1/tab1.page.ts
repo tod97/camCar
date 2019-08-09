684034926b2ad69bafa78faacbfe31f84f2b1004
Francesco <todinofrancesco97@gmail.com>
2019-08-09 22:54:17 +0200

    move Joystick

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -9,9 +9,49 @@ import { HttpRequestsService } from '../http-requests.service';
 })
 export class Tab1Page {
 
-  constructor(private req: HttpRequestsService) { }
+  controller: any;
+  controllerData: any;
 
-  ionViewDidEnter() { }
+  constructor(private req: HttpRequestsService) {
+  }
+
+  ionViewDidEnter() {
+    this.controller = require('nipplejs').create({
+      color: 'black',
+      mode: 'static',
+      position: {left: '50%', top: '50%'},
+      size: 200,
+      zone: document.getElementById('zone_joystick')
+    });
+    this.controller.on('move', (evt, data) => {
+      switch (evt.type) {
+        case 'move':
+          this.req.socketSend('move', this.calculatePower(data.distance, data.direction.x, data.direction.y, data.angle.radian));
+          break;
+      }
+    });
+  }
+
+  calculatePower(distance: number, x: string, y: string, radian: number) {
+    let dLeft: number, dRight: number;
+    if (x === 'right' && y === 'up') {
+      dLeft = distance;
+      dRight = distance * Math.sin(radian);
+    }
+    if (x === 'left' && y === 'up') {
+      dLeft = distance * Math.sin(radian);
+      dRight = distance;
+    }
+    if (x === 'left' && y === 'down') {
+      dLeft = distance * Math.sin(radian);
+      dRight = -distance;
+    }
+    if (x === 'right' && y === 'down') {
+      dLeft = -distance;
+      dRight = distance * Math.sin(radian);
+    }
+    return {dLeft, dRight};
+  }
 
   connect() {
     this.req.socketConnect();
